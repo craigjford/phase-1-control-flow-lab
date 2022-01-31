eae04defcbd7c787cc78cd9239c37a9647fef24d
Craig Ford <craig_ford@icloud.com>
2022-01-31 16:32:03 -0500

    commit tree b775c1c18c4a25807c7e808137b9e6f52decef1f

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feetTraveled){
+    if (feetTraveled <= 400) {
+        return `This one is on me!`;
+    } else if (feetTraveled > 2500) {
+        return `No can do.`;
+    }
+    else if (feetTraveled > 2000) {
+        return `I will gladly take your thirty bucks.`;
+    } else {
+        return `Please call our dispatcher`;
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(isCity){
+    return ((isCity ==='NYC') ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+  switch(tip) {
+    case "generous":
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';  
+  }
 }
+
+scuberGreetingForFeet(199);
+
+ternaryCheckCity('NYC');
+
+switchOnCharmFromTip('generous');
